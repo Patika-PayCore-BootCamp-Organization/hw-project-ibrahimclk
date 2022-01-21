5a630360656ef1bce8a5185608b4607b259f00c7
ibrahimclk <ibrhmclk.95@gmail.com>
2022-01-21 20:17:07 +0300

    project  base initialized

---
+++ src/main/java/com/patika/paycore/DebitCardManagement/DebitCardManagementApplication.java
@@ -0,0 +1,13 @@
+package com.patika.paycore.DebitCardManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DebitCardManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DebitCardManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/patika/paycore/DebitCardManagement/DebitCardManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patika.paycore.DebitCardManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DebitCardManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
