e24650abae11accc88abf0f0bd52cf0fd767ffa3
ibrahimclk <ibrhmclk.95@gmail.com>
2022-02-09 22:59:48 +0300

    created new exception

---
+++ src/main/java/com/patika/paycore/DebitCardManagement/exception/InvalidRequestException.java
@@ -0,0 +1,8 @@
+package com.patika.paycore.DebitCardManagement.exception;
+
+public class InvalidRequestException extends RuntimeException {
+    public InvalidRequestException(String message) {
+        super(message);
+    }
+
+}
