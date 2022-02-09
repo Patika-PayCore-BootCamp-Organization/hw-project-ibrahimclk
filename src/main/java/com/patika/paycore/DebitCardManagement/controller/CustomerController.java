039adcd6b4edc7cc4f1f260232a7b9ef08477e3c
ibrahimclk <ibrhmclk.95@gmail.com>
2022-02-09 22:58:59 +0300

    updated CustomerController

--- src/main/java/com/patika/paycore/DebitCardManagement/controller/CustomerController.java
+++ src/main/java/com/patika/paycore/DebitCardManagement/controller/CustomerController.java
@@ -1,15 +1,62 @@
 package com.patika.paycore.DebitCardManagement.controller;
 
 
+import com.patika.paycore.DebitCardManagement.exception.InvalidRequestException;
+import com.patika.paycore.DebitCardManagement.model.Customer;
+import com.patika.paycore.DebitCardManagement.model.CustomerDTO;
+import com.patika.paycore.DebitCardManagement.model.mapper.CustomerMapper;
+import com.patika.paycore.DebitCardManagement.service.CustomerService;
 import lombok.RequiredArgsConstructor;
+import org.mapstruct.factory.Mappers;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import javax.validation.constraints.Min;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Validated
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/customer")
 public class CustomerController {
+
+    private final CustomerService customerService;
+    private static final CustomerMapper CUSTOMER_MAPPER = Mappers.getMapper(CustomerMapper.class);
+
+    @GetMapping
+    public String welcome() {
+        return "Welcome to Customer Service!";
+    }
+
+    @GetMapping(value = "/all")
+    public List<CustomerDTO> getAllCustomers() {
+        List<Customer> allCustomers = customerService.getAllCustomers();
+        return allCustomers.stream().map(CUSTOMER_MAPPER::toDto).collect(Collectors.toList());
+    }
+
+    @GetMapping(value = "/{id}")
+    public CustomerDTO getCustomer(@PathVariable @Min(1) Integer id) {
+        return CUSTOMER_MAPPER.toDto(customerService.getCustomer(id));
+    }
+
+    @PostMapping(value = "/create")
+    public void saveCustomer(@Valid @RequestBody CustomerDTO customer) {
+        customerService.addCustomer(CUSTOMER_MAPPER.toEntity(customer));
+    }
+
+    @PutMapping(value = "/update")
+    public CustomerDTO updateCustomer(@Valid @RequestBody Customer customer) {
+        if (customer.getId() == null) {
+            throw new InvalidRequestException("Customer id can not be null for update!");
+        }
+        return CUSTOMER_MAPPER.toDto(customerService.updateCustomer(customer));
+    }
+
+    @DeleteMapping(value = "/delete")
+    public boolean deleteCustomer(@RequestParam @Min(1) Integer id) {
+        return customerService.deleteCustomer(id);
+    }
 
 }
