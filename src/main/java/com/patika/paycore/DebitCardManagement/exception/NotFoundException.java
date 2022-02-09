87f712e277f7c33ea5d0808dd2d6b995904b077f
ibrahimclk <ibrhmclk.95@gmail.com>
2022-02-09 21:36:48 +0300

    created exception package and added NotFoundException

---
+++ src/main/java/com/patika/paycore/DebitCardManagement/exception/NotFoundException.java
@@ -0,0 +1,9 @@
+package com.patika.paycore.DebitCardManagement.exception;
+
+public class NotFoundException extends RuntimeException{
+
+    public NotFoundException(String message) {
+        super(message + " not found!");
+    }
+
+}
